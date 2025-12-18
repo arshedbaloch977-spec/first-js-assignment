dd968e78f78594f28262325bf3da2465a383bcaf
Muzuu <arshedbaloch977@gmail.com>
2025-12-18 18:36:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+alert("Welcome to my website!");
+alert("Hello!\nWelcome to JavaScript Learning");
+
+var username = "student01";
+var myName = "Muzuu";
+
+console.log("Username:", username);
+console.log("Full Name:", myName);
+
+
+var message = "Hello World";
+alert(message);
+console.log(message);
+
+var age = "18";
+var course = "Web Development";
+
+alert("Name: " + myName);
+alert("Age: " + age);
+alert("Course: " + course);
+
+console.log("Bio Data shown in alerts");
+
+
+var email = "Muzuu@gmail.com";
+alert("My email address is " + email);
+console.log(email);
+
+
+var book = "A smarter way to learn JavaScript";
+alert("I am learning from the book " + book);
+console.log(book);
+
+var a = 1, b = 2, c = 3;
+
+
+var name;
+var _age;
+var $price;
+var user1;
+var first_name;
+
+// Illegal variables (commented)
+// var 1user;
+// var user-name;
+// var var;
+// var alert;
+// var first name;
+
+console.log("Legal and Illegal variables assignment done");
+
+
+  var num1 = 10;
+  var num2 = 5;
+
+  var userName = prompt("Enter your name");
+  alert("Welcome " + userName);
+  console.log("User greeted");
+
+
+  var number = prompt("Enter a number for table", 5);
+
+  document.write("<h2>Table of " + number + "</h2>");
+  for (var i = 1; i <= 10; i++) {
+    document.write(number + " x " + i + " = " + (number * i) + "<br>");
+  }
+
+  console.log("Table printed");
